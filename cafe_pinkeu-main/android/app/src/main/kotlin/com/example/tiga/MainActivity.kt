af800fc1765ce62c986e79468f4730b25d7aebe7
Eva Riyanti <129642247+Riyanti08@users.noreply.github.com>
2024-12-18 15:48:37 +0700

    Delete cafe_pinkeu-main directory

--- cafe_pinkeu-main/android/app/src/main/kotlin/com/example/tiga/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tiga
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- cafe_pinkeu-main/lib/core/core.dart
+++
@@ -1,3 +0,0 @@
-export 'assets/assets.gen.dart';
-export 'constants/colors.dart';
-export 'constants/size.dart';
